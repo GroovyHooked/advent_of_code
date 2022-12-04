faef3565064addadc1a924ef72e5e130e4bf1b06
GroovyHooked <thomas.cariot@dailymotion.com>
2022-12-04 20:55:02 +0100

    Day 1 / Part 2

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const data = document.querySelector('pre').innerText;
+const input = document.querySelector('pre');
+const data = input.innerText;
 const regex = /(\n\d+)+/gm;
 const rawFoodPerElf = data.match(regex);
+const arrayOfTotalCalories = [];
+const topValues = [];
 
-const computeTotalCaloriesPerElf = (element) => {
+for (let i = 0; i < 3; i++) {
+  const { max, index } = computeMaxCalories(rawFoodPerElf);
+  topValues.push(max);
+  i === 0 ? rawFoodPerElf.splice(index, 1) : rawFoodPerElf.splice(index - 1, 1);
+}
+
+console.log(
+  'Top values total:',
+  topValues.reduce((acc, curr) => {
+    return acc + curr;
+  })
+);
+
+function computeMaxCalories(arrayOfValues) {
+  const max = arrayOfValues.reduce((a, b) => {
+    if (typeof a === 'string')
+      arrayOfTotalCalories.push(computeTotalCaloriesPerElf(a));
+    if (typeof b === 'string')
+      arrayOfTotalCalories.push(computeTotalCaloriesPerElf(b));
+    return Math.max(
+      computeTotalCaloriesPerElf(a),
+      computeTotalCaloriesPerElf(b)
+    );
+  });
+  return { max, index: arrayOfTotalCalories.indexOf(max) };
+}
+
+function computeTotalCaloriesPerElf(element) {
   if (typeof element === 'number') return element;
-  return element.split('\n').reduce((acc, curr) => {
+  const array = element.split('\n');
+  return array.reduce((acc, curr) => {
     return Number(acc) + Number(curr);
   }, 0);
-};
-
-const max = rawFoodPerElf.reduce((a, b) => {
-  return Math.max(computeTotalCaloriesPerElf(a), computeTotalCaloriesPerElf(b));
-});
-
-console.log('Max calories: ', max);
+}
