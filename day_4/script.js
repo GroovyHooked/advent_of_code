eb3f97bae1b166fe3551bb0a6491a4180dbf5e11
GroovyHooked <thomas.cariot@dailymotion.com>
2022-12-04 21:05:03 +0100

    Day 4 / Part 2

--- day_4/script.js
+++ day_4/script.js
@@ -1,7 +1,8 @@
 const data = document.querySelector('pre').innerText;
 const arrayOfSections = data.split('\n').filter((el) => !onlySpaces(el));
 let howManyShift = 0;
-arrayOfSections.forEach((line, index) => {
+
+arrayOfSections.forEach((line) => {
   const newline = line
     .split('')
     .filter((el) => el !== ' ')
@@ -9,15 +10,33 @@ arrayOfSections.forEach((line, index) => {
   const [range1, range2] = newline.split(',');
   const [start1, end1] = range1.split('-');
   const [start2, end2] = range2.split('-');
-  if (
-    (Number(start1) <= Number(start2) && Number(end1) >= Number(end2)) ||
-    (Number(start2) <= Number(start1) && Number(end2) >= Number(end1))
-  ) {
-    howManyShift++;
+
+  let hoursOverlaped = 0;
+  const isFirstLonger =
+    Number(end1) - Number(start1) > Number(end2) - Number(start2);
+  const array = [];
+  if (isFirstLonger) {
+    for (let y = Number(start2); y <= Number(end2); y++) {
+      array.push(y);
+    }
+    for (let i = Number(start1); i <= Number(end1); i++) {
+      if (array.includes(i)) {
+        hoursOverlaped++;
+      }
+    }
+  } else {
+    for (let y = Number(start1); y <= Number(end1); y++) {
+      array.push(y);
+    }
+    for (let i = Number(start2); i <= Number(end2); i++) {
+      if (array.includes(i)) {
+        hoursOverlaped++;
+      }
+    }
   }
-  arrayOfSections[index] = newline;
+  hoursOverlaped !== 0 ? howManyShift++ : null;
 });
-console.log({ howManyShift });
+console.log({ arrayOfSections, howManyShift });
 
 function onlySpaces(str) {
   return str.trim().length === 0;
