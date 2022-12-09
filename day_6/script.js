9dbacb792899f481ff84438e166f9aaa6b17cfce
GroovyHooked <thomas.cariot@dailymotion.com>
2022-12-09 15:59:48 +0100

    Day 6 / Part 2

--- day_6/script.js
+++ day_6/script.js
@@ -1,9 +1,9 @@
 const data = document.querySelector('pre').innerText;
 
 for (let i = 0; i <= data.length; i++) {
-  const values = data.split('').splice(i, 4);
+  const values = data.split('').splice(i, 14);
   if (values.length === new Set(values).size) {
-    console.log(i + 4);
+    console.log(i + 14);
     break;
   }
 }
